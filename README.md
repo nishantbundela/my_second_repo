e1745c905ad8257c84602b553504e181e9945b14
nishantbundela <nishantbundela@gmail.com>
2021-05-04 15:38:52 +0530

    Unix Project

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Unix Workbench project: Guessinggame
 This make was run on:
-Tue May  4 14:38:23 IST 2021
+Tue May  4 15:38:23 IST 2021
 
 The number of lines in guessinggame.sh are:
 25
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
-a=0
+response=0
 b=$(ls | wc -l)
-while [[ $a -ne $b ]]
-do
-	echo "Guess the number of files in this directory:"
-	read response
-	a=$response
-	if [[ $a -eq $b ]]
-		then
-		echo "Congratulations! You have guessed correctly, the no. of files in this directory is indeed $a"
+function guess {
+	if [[ $1 -eq $2 ]]
+	then
+	echo "Congratulations! You have guessed correctly, the no. of files in this directory is indeed $a"
+	elif [[ $1 -gt $2 ]]
+	then
+	echo "Your guess is greater than the number of files."
 	else
-		if [[ $a -lt $b ]]
-		then
-		echo "Your guess is less than the number of files."
-		else
-		echo "Your guess is greater than the number of files."
-		fi
-		echo "Try again!"
-	echo "------------------------------------------"
+	echo "Your guess is less than the number of files."
 	fi
+	echo "----------------------------------------------"
+}
+
+while [[ $response -ne $b ]]
+do
+	echo "Guess the number of files in this directory"
+	read response
+	guess $response $b
 done
 
